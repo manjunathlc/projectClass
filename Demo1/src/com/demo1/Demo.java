c4acd26413e4d0c6cfcb7b87fec31d781e814bba
manjunathlc <www.manjuchitagubbi96@gmail.com>
2024-05-08 20:36:25 +0530

    created in Eclipse

---
+++ Demo1/src/com/demo1/Demo.java
@@ -0,0 +1,14 @@
+package com.demo1;
+
+public class Demo {
+	public static void main(String[] args) {
+		System.out.println("create remote Repo local");
+		System.out.println("open local repo as workspace");
+		System.out.println("Create project ");
+		System.out.println("after developing right click on project");
+		System.out.println("show terminal and open git bash");
+		System.out.println("use the three commands to push from local to remote");
+		
+	}
+
+}
